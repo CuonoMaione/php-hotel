13201d7cc71e15dd233719fe2fed651247fc21cc
CuonoMaione <cuonomaione99@outlook.it>
2023-07-04 10:07:14 +0200

    add table

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <!--BootStrap v5.3 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
-<body class="d-flex">
+<body>
     <?php
 
     $hotels = [
@@ -50,17 +50,32 @@
 
     ];
 
-
-    foreach ($hotels as $key => $value) { ?>
-        <section class="m-5">
-        <h1> <?php echo "$value[name]" ?> </h1>
-        <p><?php echo " $value[description]" ?></p>
-        <p><?php echo "Parking: $value[parking]" ?></p>
-        <p><?php  echo "Vote : $value[vote] " ?></p>
-        <p><?php  echo "Distance to center : $value[distance_to_center] km"?></p>
-       </section> 
+    ?>
+    
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Parking</th>
+      <th scope="col">Vote</th>
+      <th scope="col">Distance to center</th>
+    </tr>
+</thead> 
+<tbody>
+    <?php foreach ($hotels as $key => $value) { ?>
+      <tr>
+        <th scope="row"><?php echo "$value[name]" ?> </th>
+        <td><?php echo " $value[description]" ?></td>
+        <td><?php echo "Parking: $value[parking]" ?></td>
+        <td><?php  echo "Vote : $value[vote] " ?></td>
+        <td><?php  echo "Distance to center : $value[distance_to_center] km"?></td>
+     </tr>
     <?php } ?>
-
+    </tbody>
+    </table> 
+    
 
 </body>
 </html>
+       
